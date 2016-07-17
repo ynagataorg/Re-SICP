a3d69669cc13a59b0556edecb912cdcb6da3251e
ynagata.org <strangamo1986@gmail.com>
2016-07-17 14:52:33 +0900

    Section 1.1.*; Exercise 1.7 (SqrtIter)

---
+++ Chapter1-Functions/ConsoleApp1/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Math;
+
+namespace ConsoleApp1
+{
+    public static class Extensions
+    {
+        public static decimal Square(decimal target)
+        {
+            return target * target;
+        }
+
+        // Section 1.1.7:
+        public static decimal MySqrt(decimal target)
+        {
+            //return MySqrtIter(1m, target);
+            return MySqrtIter2(1m, 1m / target, target);
+        }
+
+        // Section 1.1.7:
+        private static decimal MySqrtIter(decimal guessValue, decimal target)
+        {
+            Func<decimal, decimal, bool> goodEnough = (guess, x) => Abs(guess * guess - x) < 1e-10m;
+            Func<decimal, decimal, decimal> improve = (guess, x) => (guess + (x / guess)) / 2;
+            return goodEnough(guessValue, target)
+                ? guessValue
+                : MySqrtIter(improve(guessValue, target), target);
+        }
+
+        // Exercise 1.7:
+        private static decimal MySqrtIter2(decimal guessValue, decimal oldGuessValue, decimal target)
+        {
+            Func<decimal, decimal, bool> goodEnough = (guess, oldGuess) => Abs(guess - oldGuess) / guess < 1e-10m;
+            Func<decimal, decimal, decimal> improve = (guess, x) => (guess + (x / guess)) / 2;
+            return goodEnough(guessValue, oldGuessValue)
+                ? guessValue
+                : MySqrtIter2(improve(guessValue, target), guessValue, target);
+        }
+    }
+}
---
+++ Chapter1-Functions/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var targets = new[] {2m, 3m, 5m, 1e-10m, 1e-20m, 1e10m, 1e16m};
+            foreach (var target in targets)
+            {
+                var sqrt = Extensions.MySqrt(target);
+                Console.WriteLine($"MySqrt({target}) = {sqrt}");
+                Console.WriteLine($"Square(MySqrt({target})) = {Extensions.Square(sqrt)}");
+                Console.WriteLine($"Error: {Math.Abs(Extensions.Square(sqrt) - target)}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
