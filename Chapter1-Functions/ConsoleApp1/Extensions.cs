6d78a794b166f681e6d7eb6a634191044c82d30c
ynagata.org <strangamo1986@gmail.com>
2016-07-17 16:42:40 +0900

    Exercise 1.8 (CbrtIter)

--- Chapter1-Functions/ConsoleApp1/Extensions.cs
+++ Chapter1-Functions/ConsoleApp1/Extensions.cs
@@ -17,6 +17,17 @@ namespace ConsoleApp1
             return MySqrtIter2(1m, 1m / target, target);
         }
 
+        public static decimal Cube(decimal target)
+        {
+            return target * target * target;
+        }
+
+        // Exercise 1.8:
+        public static decimal MyCbrt(decimal target)
+        {
+            return MyCbrtIter(1m, 1m / target, target);
+        }
+
         // Section 1.1.7:
         private static decimal MySqrtIter(decimal guessValue, decimal target)
         {
@@ -35,6 +46,16 @@ namespace ConsoleApp1
             return goodEnough(guessValue, oldGuessValue)
                 ? guessValue
                 : MySqrtIter2(improve(guessValue, target), guessValue, target);
+        }
+
+        // Exercise 1.8:
+        private static decimal MyCbrtIter(decimal guessValue, decimal oldGuessValue, decimal target)
+        {
+            Func<decimal, decimal, bool> goodEnough = (guess, oldGuess) => Abs(guess - oldGuess) / guess < 1e-10m;
+            Func<decimal, decimal, decimal> improve = (guess, x) => (2 * guess + x / Square(guess)) / 3;
+            return goodEnough(guessValue, oldGuessValue)
+                ? guessValue
+                : MyCbrtIter(improve(guessValue, target), guessValue, target);
         }
     }
 }
--- Chapter1-Functions/ConsoleApp1/Program.cs
+++ Chapter1-Functions/ConsoleApp1/Program.cs
@@ -6,6 +6,12 @@ namespace ConsoleApp1
     {
         private static void Main(string[] args)
         {
+            TryMySqrt();
+            TryMyCbrt();
+        }
+
+        private static void TryMySqrt()
+        {
             var targets = new[] {2m, 3m, 5m, 1e-10m, 1e-20m, 1e10m, 1e16m};
             foreach (var target in targets)
             {
@@ -13,6 +19,19 @@ namespace ConsoleApp1
                 Console.WriteLine($"MySqrt({target}) = {sqrt}");
                 Console.WriteLine($"Square(MySqrt({target})) = {Extensions.Square(sqrt)}");
                 Console.WriteLine($"Error: {Math.Abs(Extensions.Square(sqrt) - target)}");
+                Console.WriteLine();
+            }
+        }
+
+        private static void TryMyCbrt()
+        {
+            var targets = new[] { 2m, 8m, 27m, 1e-12m, 1e-21m, 1e9m, 1e12m };
+            foreach (var target in targets)
+            {
+                var cbrt = Extensions.MyCbrt(target);
+                Console.WriteLine($"MyCbrt({target}) = {cbrt}");
+                Console.WriteLine($"Cube(MyCbrt({target})) = {Extensions.Cube(cbrt)}");
+                Console.WriteLine($"Error: {Math.Abs(Extensions.Cube(cbrt) - target)}");
                 Console.WriteLine();
             }
         }
