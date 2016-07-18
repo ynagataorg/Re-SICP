91ce22aedca25a8f2e6d6bc4ff090c3caedb7114
ynagata.org <strangamo1986@gmail.com>
2016-07-18 17:55:28 +0900

    Section 1.2.1; Exercise 1.10 (Ackermann)

---
+++ Chapter1-Functions/ConsoleApp1/Extensions.CalcFactorial.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp1
+{
+    public static partial class Extensions
+    {
+        public static uint Ackermann(uint m, uint n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (m == 0)
+            {
+                return 2 * n;
+            }
+            if (n == 1)
+            {
+                return 2;
+            }
+
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+
+        public static uint Factorial(uint n)
+        {
+            // linear recursive process.
+            return n == 0
+                ? 1
+                : n * Factorial(n - 1);
+        }
+
+        public static uint Factorial2(uint n)
+        {
+            return FactIter(1, 1, n);
+        }
+
+        private static uint FactIter(uint product, uint count, uint maxCount)
+        {
+            // linear iterative process.
+            return (count > maxCount)
+                ? product
+                : FactIter(count * product, count + 1, maxCount);
+        }
+    }
+}
--- Chapter1-Functions/ConsoleApp1/Program.cs
+++ Chapter1-Functions/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -6,13 +7,15 @@ namespace ConsoleApp1
     {
         private static void Main(string[] args)
         {
-            TryMySqrt();
-            TryMyCbrt();
+            //TryMySqrt();
+            //TryMyCbrt();
+            TryMyFact();
+            TryAck();
         }
 
         private static void TryMySqrt()
         {
-            var targets = new[] {2m, 3m, 5m, 1e-10m, 1e-20m, 1e10m, 1e16m};
+            var targets = new[] { 2m, 3m, 5m, 1e-10m, 1e-20m, 1e10m, 1e16m };
             foreach (var target in targets)
             {
                 var sqrt = Extensions.MySqrt(target);
@@ -33,6 +36,37 @@ namespace ConsoleApp1
                 Console.WriteLine($"Cube(MyCbrt({target})) = {Extensions.Cube(cbrt)}");
                 Console.WriteLine($"Error: {Math.Abs(Extensions.Cube(cbrt) - target)}");
                 Console.WriteLine();
+            }
+        }
+
+        private static void TryMyFact()
+        {
+            var targets = new uint[] { 0, 1, 2, 3, 4, 5, 6 };
+            foreach (var target in targets)
+            {
+                Console.WriteLine($"Factorial({target}) = {Extensions.Factorial(target)}");
+                Console.WriteLine($"Factorial2({target}) = {Extensions.Factorial2(target)}");
+                Console.WriteLine();
+            }
+        }
+
+        private static void TryAck()
+        {
+            Console.WriteLine($"Ack(1, 10) = {Extensions.Ackermann(1, 10)}");
+            Console.WriteLine($"Ack(2, 4) = {Extensions.Ackermann(2, 4)}");
+            Console.WriteLine($"Ack(3, 3) = {Extensions.Ackermann(3, 3)}");
+
+            foreach (var ii in Enumerable.Range(0, 10))
+            {
+                Console.WriteLine($"Ack(1, {ii}) = {Extensions.Ackermann(1, (uint)ii)}");
+            }
+            foreach (var ii in Enumerable.Range(0, 5))
+            {
+                Console.WriteLine($"Ack(2, {ii}) = {Extensions.Ackermann(2, (uint)ii)}");
+            }
+            foreach (var ii in Enumerable.Range(0, 4))
+            {
+                Console.WriteLine($"Ack(3, {ii}) = {Extensions.Ackermann(3, (uint)ii)}");
             }
         }
     }
