fbd0e9cb065fb0db4c5a4cb206081e58c862e0ea
ynagata.org <strangamo1986@live.jp>
2017-10-28 14:52:48 +0900

    ex.1.41, 1.42 implemented.

--- Lisp/Chapter1-Functions/1-3.scm
+++ Lisp/Chapter1-Functions/1-3.scm
@@ -725,3 +725,30 @@ my-sqrt-newton
 (solve-cubic 4 -3 -18) ; (x+3)^2(x-2)=0
 2.000000000000059
 
+; ex.1.41
+(define (double f)
+  (lambda (x) (f (f x))))
+double
+
+((double inc) 5)
+7 ; 5 + 2
+
+(((double double) inc) 5)
+9 ; 5 + 2^2
+
+(((double (double double)) inc) 5)
+21 ; 5 + (2^2)^2
+
+(((double (double (double double))) inc) 5)
+261 ; 5 + ((2^2)^2)^2
+
+((((double double) (double double)) inc) 5)
+261 ; 5 + ((2^2)^(2^2))
+
+; ex.1.42
+(define (compose f g)
+  (lambda (x) (f (g x))))
+compose
+
+((compose square inc) 6)
+49
