cb727b169cdb995a952c363565e075646f741a0f
ynagata.org <strangamo1986@live.jp>
2018-01-27 17:03:14 +0900

    ex.2.63 Implemented.

--- Lisp/Chapter2-Objects/2-3.scm
+++ Lisp/Chapter2-Objects/2-3.scm
@@ -205,3 +205,67 @@ s5 ;'(1 3 5)
 '(1 3 5)
 (union-set-ord (adjoin-set-ord 2 s5) (adjoin-set-ord 4 s5))
 '(1 2 3 4 5)
+
+; ex.2.63
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+(define (make-leaf entry)
+  (list entry '() '()))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1
+                     (right-branch tree))))))
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list
+                             (right-branch tree)
+                             result-list)))))
+  (copy-to-list tree '()))
+
+(define t1
+  (make-tree 7
+             (make-tree 3
+                        (make-leaf 1)
+                        (make-leaf 5))
+             (make-tree 9
+                        '()
+                        (make-leaf 11))))
+(tree->list-1 t1)
+(tree->list-2 t1)
+; '(1 3 5 7 9 11)
+
+(define t2
+  (make-tree 3
+             (make-leaf 1)
+             (make-tree 7
+                        (make-leaf 5)
+                        (make-tree 9
+                                   '()
+                                   (make-leaf 11)))))
+(tree->list-1 t2)
+(tree->list-2 t2)
+; '(1 3 5 7 9 11)
+
+(define t3
+  (make-tree 5
+             (make-tree 3
+                        (make-leaf 1)
+                        '())
+             (make-tree 9
+                        (make-leaf 7)
+                        (make-leaf 11))))
+(tree->list-1 t3)
+(tree->list-2 t3)
+; '(1 3 5 7 9 11)
+
